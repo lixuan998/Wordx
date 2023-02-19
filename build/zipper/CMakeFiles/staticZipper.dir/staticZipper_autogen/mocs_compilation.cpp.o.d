d8890736d254882a942000d743dd2c5c19d48c0d
climatex <1714500849@qq.com>
2023-02-20 00:19:34 +0800

    修复

--- build/zipper/CMakeFiles/staticZipper.dir/staticZipper_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-zipper/CMakeFiles/staticZipper.dir/staticZipper_autogen/mocs_compilation.cpp.o: \
- /home/climatex/Lx_Workspace/Qt_workspace/wordx/build/zipper/staticZipper_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
